88a3eb3299c0b07de68e1435f64a2feb33f82d90
Will Aucoin <aucoinwill@gmail.com>
2016-09-14 11:24:02 -0300

    Hopefully this works :)

---
+++ Car/Car.py
@@ -0,0 +1,18 @@
+"""
+How fast is a car traveling if it goes 50 miles in 2 hours?
+
+Output: a number giving the speed in miles per hour
+
+Input: the distance and time the car has traveled
+
+Process: speed = distance / time
+"""
+#Input
+distance = 50
+time = 2
+
+#Processing
+speed = distance / time
+
+#Output
+print("The speed is:",speed,"Mph")
